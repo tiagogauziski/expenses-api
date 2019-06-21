4b8c70eb3433fd50e71b2c41a7cdb693f134f316
Tiago Gauziski <tiago.gauziski@thomsonreuters.com>
2019-06-21 18:06:26 -0300

    initial push

---
+++ Expenses.API/Controllers/ExpenseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.API.Controllers
+{
+    /// <summary>
+    /// Expenses operations
+    /// </summary>
+    [Route("expenses")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        /// <summary>
+        /// Initialize ExpenseController
+        /// </summary>
+        public ExpenseController()
+        {
+
+        }
+
+        /// <summary>
+        /// Create an expense entry
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("")]
+        //[ProducesResponseType(typeof(JsonDiffStatusViewModel), StatusCodes.Status200OK)]
+        //[ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
