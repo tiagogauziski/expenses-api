08a00fce95f045139b1cc9a15bcd070b38a95535
Tiago Gauziski <tiago.gauziski@gmail.com>
2020-03-16 22:20:50 +1300

    testing

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ TAG=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
 REPOSITORY_PROJECT=tiagogauziski/expenses-api
-echo "$REPOSITORY_PROJECT" 
 
 # Remove a leading v from the major version number (e.g. if the tag was v1.0.0)
 IFS='.' read -r -a tag_array <<< "$TAG"
@@ -16,8 +15,8 @@ BUILD=${tag_array[2]}
 SEMVER="$MAJOR.$MINOR.$BUILD"
 
 # Build the Docker images
-docker build -f src/Expenses.API/Dockerfile src/. -t $REPOSITORY_PROJECT:$TAG
-docker tag $REPOSITORY_PROJECT:$TAG $REPOSITORY_PROJECT:latest
+docker build -f src/Expenses.API/Dockerfile src/. -t $REPOSITORY_PROJECT:$SEMVER
+docker tag $REPOSITORY_PROJECT:$SEMVER $REPOSITORY_PROJECT:latest
 
 # Login to Docker Hub and upload images
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
