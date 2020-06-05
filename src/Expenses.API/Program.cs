d8767bc4327f3025dc603bc616308f94e357be97
Tiago Gauziski <tiago.gauziski@gmail.com>
2020-06-05 20:39:37 +1200

    add migration when starting up application.

--- src/Expenses.API/Program.cs
+++ src/Expenses.API/Program.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore;
+﻿using Expenses.Infrastructure.SqlServer;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Serilog;
-using System.Diagnostics;
+using System;
 
 namespace Expenses.API
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
-            //Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-            //Activity.ForceDefaultIdFormat = true;
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                // This might not be suitable for a production use, as it might not scale well,
+                // due to potential race conditions to execute migrations.
+                // For production use, make sure to have this in a separate pre-deployment process
+                .MigrateDatabase<ExpensesContext>() 
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -20,6 +27,35 @@ namespace Expenses.API
                 .UseStartup<Startup>()
                 .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
                     .ReadFrom.Configuration(hostingContext.Configuration));
+    }   
 
+    /// <summary>
+    /// Extension method to enhance the WebHostBuilder
+    /// </summary>
+    internal static class ExtensionMethods
+    {
+        public static IWebHost MigrateDatabase<T>(this IWebHost webHost) where T : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogInformation("Executing database migrations...");
+
+                    var db = services.GetRequiredService<T>();
+                    db.Database.Migrate();
+
+                    logger.LogInformation("Database migrations executed successfully.");
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            return webHost;
+        }
     }
 }
